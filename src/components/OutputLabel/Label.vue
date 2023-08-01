c26c6f4be0b28c712a1f1a0f105997fcdef41adb
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-01 15:35:44 +0530

    refactor

---
+++ src/components/OutputLabel/Label.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { copyToClipboard, labelFormatter } from '../utils/UnixDateTimeFunctions';
+
+const props = defineProps({
+    key: String,
+    value: String
+})
+
+const handleClick = (value) => {
+    copyToClipboard(value)
+}
+</script>
+
+
+<template>
+    <div role="button" @click="handleClick(value)" class="card p-1 flex flex-row">
+        <strong> {{ labelFormatter(key) }} </strong> : {{ value }}
+    </div>
+</template>
---
+++ src/pages/DateToUnix/index.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref } from 'vue';
+import { formatDates, labelFormatter } from '../../components/utils/UnixDateTimeFunctions';
+import DateTimeConstants from '../../components/DateTimeConstants/DateTimeConstants.vue';
+import { copyToClipboard } from '../../components/utils/UnixDateTimeFunctions';
+import Label from '../../components/OutputLabel/Label.vue';
+
+const unix = ref()
+const data = ref()
+
+const handleChange = () => {
+    data.value = formatDates(unix.value)
+}
+
+const handleClick = (value) => {
+    copyToClipboard(value)
+}
+
+
+</script>
+
+<template>
+    <div class="grid bg-light">
+        <div class="block card block1">
+            <div class="p-3">
+                <h2>
+                    <strong>
+                        Unix to date converter
+                    </strong>
+                </h2>
+                <input v-model="unix" @input="handleChange" type="text" class="form-control"
+                    placeholder="Enter unix timestamp">
+
+                <div class="mt-2 p-2">
+                    <DateTimeConstants />
+                </div>
+            </div>
+        </div>
+        <div class="block bg-light block2">
+            <div v-if="unix">
+                <div v-for="(value, key) in data" class="p-1 d-flex flex-column gap-2" :key="key">
+                    <Label :key=key :value=value />
+                    <!-- <div role="button" @click="handleClick(value)" class="card p-1 flex flex-row">
+                        <strong> {{ labelFormatter(key) }} </strong> : {{ value }}
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped src="./style.css" />
